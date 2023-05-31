a666edc14e104e374c02b3bb3ccf7269129f7116
gabriellespits <gabrielle-spits@hotmail.com>
2023-05-31 09:48:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bluetooth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothSend bluetoothSend = new BluetoothSend();
+        
+    }
+}
